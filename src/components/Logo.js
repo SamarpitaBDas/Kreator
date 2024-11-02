f58c419dede311cae9e65b9092200912e0ea6269
Samarpita Das <samarpitabdas@gmail.com>
2024-11-03 02:54:24 +0530

    fragment_look

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,54 @@
+import React from 'react';
 import edit from '../assets/logos/Edit_light.svg';
+import evolve from '../assets/logos/evolve.svg';
+import maximize from '../assets/logos/maximize.svg';
 
 function Logo() {
+    const containerStyle = {
+        marginTop:'28vh',
+        marginLeft:'31vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center', 
+        height: '40%', 
+        padding: '10px'
+    };
+
     return (
         <div 
             className="Logo" 
             style={{ 
                 backgroundColor: 'black', 
                 height: '50vh', 
-                borderRadius: '10px', 
+                borderRadius: '20px', 
                 width: '40vh', 
-                color: 'white' 
+                color: 'white',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
             }}
         >
-            <img src={edit} className="edit" alt="logo" />
+            <div style={containerStyle}>
+                <img 
+                    src={edit} 
+                    className="edit" 
+                    alt="Edit logo" 
+                    style={{ maxWidth: '100%', height: 'auto' }} 
+                />
+                <img
+                    src={evolve} 
+                    className="edit"
+                    alt="Evolve logo" 
+                    style={{ maxWidth: '100%', height: 'auto' }} 
+                />
+                <img
+                    src={maximize} 
+                    className="edit" 
+                    alt="Maximize logo" 
+                    style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
         </div>
     );
 }
